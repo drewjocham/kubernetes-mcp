bf501a900c9b55331a4ae1e15a6dbb03a5f58a40
jocham <peter.jocham@mail.schwarz>
2026-03-19 00:48:07 +0100

    updating dashboard

--- integrations/cmd/chatbridge/main.go
+++ integrations/cmd/chatbridge/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -16,11 +17,21 @@ func main() {
 	configPath := flag.String("config", "integrations/chatbridge/config.example.yaml", "path to YAML config")
 	debug := flag.Bool("debug", false, "enable debug logging")
 	logFile := flag.String("log-file", "", "optional log file path")
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Kube-Watcher ChatBridge\nUsage: chatbridge [options]\n\nOptions:\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
 	logger, err := logging.New(*debug, *logFile)
 	if err != nil {
 		slog.Error("logger init failed", "error", err)
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(*configPath); os.IsNotExist(err) {
+		logger.Error("configuration file not found", "path", *configPath)
 		os.Exit(1)
 	}
 
@@ -39,8 +50,12 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
+	logger.Info("starting chatbridge", "config", *configPath)
+
 	if err := bridge.Run(ctx); err != nil {
 		logger.Error("bridge exited with error", "error", err)
 		os.Exit(1)
 	}
+
+	logger.Info("chatbridge shut down gracefully")
 }
